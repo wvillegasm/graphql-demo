de96b1df8f05b395a51bf98b67e7c9be4d2786aa
Wilfredo Villegas <wvillegasm@gmail.com>
2016-12-26 17:35:48 -0500

    Completing the comment type and restructuring the type directory
    	renamed:    types/blog-post.js -> graphql/types/blog-post.js
    	new file:   graphql/types/comment.js

---
+++ graphql/types/comment.js
@@ -0,0 +1,23 @@
+import {
+    GraphQLObjectType,
+    GraphQLNonNull,
+    GraphQLString,
+    GraphQLID
+} from 'graphql';
+
+export default new GraphQLObjectType({
+
+    name: 'Comment',
+    fields: {
+        _id: {
+            type: new GraphQLNonNull(GraphQLID)
+        },
+        postId: {
+            type: new GraphQLNonNull(GraphQLID)
+        },
+        text: {
+            type: GraphQLString
+        }
+    }
+
+});
