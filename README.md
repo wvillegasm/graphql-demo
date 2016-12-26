495380b2743c3505d4fe3317bd3a2514ae720d77
Wilfredo Villegas <wvillegasm@gmail.com>
2016-12-26 17:10:34 -0500

    Adding the application file, Mongoose Model and GraphQL Type
    	new file:   README.md
    	modified:   app.js
    	new file:   models/blog-post.js
    	new file:   models/comments.js
    	modified:   package.json
    	new file:   types/blog-post.js

---
+++ README.md
@@ -0,0 +1,4 @@
+Schema is a group of queries and mutations
+You can think of queries as resources to retrieve from the database and mutations as any kind of update to your database
+
+
---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import mongoose from 'mongoose';
+
+
+import schema from './graphql';
+
+
+// Start the server
+const app = express(),
+    PORT = 3000;
+
+// GraphQL server route
+app.use('/graphql', graphqlHTTP(req => ({
+    schema,
+    pretty: true
+})));
+
+// Connect to mongo database
+mongoose.connect('mongodb://localhost/graphql');
+const server = app.listen(PORT, () => {
+    console.log(`Listining at port ${PORT}`);
+});
